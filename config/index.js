9a776336c3cf36464ce927b46eed24cd606909c6
guangwu <wuguang407906079@163.com>
2019-11-16 19:41:10 +0800

    修改配置

--- config/index.js
+++ config/index.js
@@ -52,7 +52,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://m.airkx.cn/',
+    assetsPublicPath: 'http://m.dakehu.airkx.cn/',
 
     /**
      * Source Maps
