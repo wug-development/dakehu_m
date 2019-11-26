64a50d7562b016d643f0e16edd14c48a48b43857
wuguang <guangwu@roseonly.com>
2019-11-26 19:16:40 +0800

    修改BUG

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     proxy: {
         '/apis': {
-            target: 'http://dakehuapi.airkx.cn/api/',//http://localhost:63844/api/
+            target: 'http://localhost:63844/api/',//http://dakehuapi.airkx.cn/api/
             changeOrigin: true,
             pathRewrite: {
                 '^/apis': ''
